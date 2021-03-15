75c43cfa1a3235ebfc882698ca5bb58c4856cf4f
Colin Holzman <colin.holzman@protonmail.com>
2021-03-15 18:01:56 -0400

    Implemented StateConfiguration.getHandlers.

--- makina-compiler/src/xyz/colinholzman/makina/StateConfiguration.kt
+++ makina-compiler/src/xyz/colinholzman/makina/StateConfiguration.kt
@@ -1,17 +1,20 @@
 package xyz.colinholzman.makina
 
 data class StateConfiguration(val states: Set<State>) {
+    //states ordered by depth, deepest first
+    private val orderedStates: List<State>
+        get() = states.sortedByDescending { it.getDepth() }
+
     fun isValid(): Boolean {
         if (states.isEmpty()) return false
-        //order states deepest first
-        val orderedStates = states.sortedBy { it.getDepth() }.reversed()
         val depthGroups = orderedStates.groupBy { it.getDepth() }
         //if any depth has more than one state then not valid
         if (depthGroups.any { it.value.size > 1 }) return false
         if (orderedStates.first().getProperAncestors(null) != orderedStates.drop(1)) return false
         return true
     }
+
     fun getHandlers(): List<Handler> {
-        return emptyList()
+        return orderedStates.flatMap { state -> state.handlers.filterIsInstance<Handler.Event>() }
     }
 }
