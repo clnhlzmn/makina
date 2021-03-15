3d5216598d6cbe31fa4a4d9fa77b79e133a4539c
Colin Holzman <colin.holzman@protonmail.com>
2021-03-15 18:04:35 -0400

    Inlined variable.

--- makina-compiler/src/xyz/colinholzman/makina/StateConfiguration.kt
+++ makina-compiler/src/xyz/colinholzman/makina/StateConfiguration.kt
@@ -7,9 +7,7 @@ data class StateConfiguration(val states: Set<State>) {
 
     fun isValid(): Boolean {
         if (states.isEmpty()) return false
-        val depthGroups = orderedStates.groupBy { it.getDepth() }
-        //if any depth has more than one state then not valid
-        if (depthGroups.any { it.value.size > 1 }) return false
+        if (orderedStates.groupBy { it.getDepth() }.any { it.value.size > 1 }) return false
         if (orderedStates.first().getProperAncestors(null) != orderedStates.drop(1)) return false
         return true
     }
