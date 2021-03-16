2b42d02272e0bbae15ddb841ea445fc054d61e8c
Colin Holzman <colin.holzman@protonmail.com>
2021-03-16 11:21:33 -0400

    Added Handler.getTargetState.

--- makina-compiler/src/xyz/colinholzman/makina/Handler.kt
+++ makina-compiler/src/xyz/colinholzman/makina/Handler.kt
@@ -3,5 +3,10 @@ package xyz.colinholzman.makina
 sealed class Handler: Node() {
     data class Entry(val action: String): Handler()
     data class Exit(val action: String): Handler()
-    data class Event(val id: String, val guard: String? = null, val action: String? = null, val target: String? = null): Handler()
+    data class Event(val id: String, val guard: String? = null, val action: String? = null, val target: String? = null): Handler() {
+        fun getTargetState(machine: Machine): State {
+            if (target == null) throw RuntimeException("handler doesn't define a transition")
+            return machine.states.find { it.id == target } ?: throw RuntimeException("target state not found")
+        }
+    }
 }
---
+++ makina-compiler/test/xyz/colinholzman/makina/HandlerTest.kt
@@ -0,0 +1,18 @@
+package xyz.colinholzman.makina
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class HandlerTest {
+    @Test
+    fun getTargetState() {
+        val s1 = State("s1")
+        val s11 = State("s11", parentId = "s1")
+        val s12 = State("s12", parentId = "s1")
+        val handler = Handler.Event("foo", target = "s2")
+        val s111 = State("s111", parentId = "s11", handlers = listOf(handler))
+        val s2 = State("s2")
+        val machine = Machine("test", listOf(s1, s11, s12, s111, s2))
+        assertEquals(s2, handler.getTargetState(machine))
+    }
+}
