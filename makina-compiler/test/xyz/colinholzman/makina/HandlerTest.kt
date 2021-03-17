85667eaaeb62b2c26549867cd988f17bce4b04fe
Colin Holzman <colin.holzman@protonmail.com>
2021-03-17 08:22:53 -0400

    Added failing test for target state selection.

--- makina-compiler/test/xyz/colinholzman/makina/HandlerTest.kt
+++ makina-compiler/test/xyz/colinholzman/makina/HandlerTest.kt
@@ -15,4 +15,17 @@ internal class HandlerTest {
         val machine = Machine("test", listOf(s1, s11, s12, s111, s2))
         assertEquals(s2, handler.getTargetState(machine))
     }
+
+    @Test
+    fun getMaybeAmbiguousTargetState() {
+        val machine = Parse.fileFromString("machine foo; " +
+                "state bar { state foo { }                state baz { } } " +
+                "state qux { state foo { on foo -> baz; } state baz { } }")
+        val bar_foo = machine.states.find { it.parent?.id == "bar" && it.id == "foo" }!!
+        val qux_foo = machine.states.find { it.parent?.id == "qux" && it.id == "foo" }!!
+        val bar_baz = machine.states.find { it.parent?.id == "bar" && it.id == "baz" }!!
+        val qux_baz = machine.states.find { it.parent?.id == "qux" && it.id == "baz" }!!
+        val handler = qux_foo.handlers.first() as Handler.Event
+        assertEquals(qux_baz, handler.getTargetState(machine))
+    }
 }
