f8fdd9cda3c88143a79f7443744dd7d864588647
Colin Holzman <colin.holzman@protonmail.com>
2021-03-17 07:26:02 -0400

    Added to TestStates.

--- makina-compiler/test/xyz/colinholzman/makina/TestStates.kt
+++ makina-compiler/test/xyz/colinholzman/makina/TestStates.kt
@@ -2,43 +2,31 @@ package xyz.colinholzman.makina
 
 class TestStates {
     companion object {
-        //               s1            s2
-        //             /   \           |
-        //           /      \         s21
+        //               s1            s2      s3
+        //             /   \           |      /  \
+        //           /      \         s21   s2   s12
         //         s11      s12
         //        /  \      /  \
         //     s111 s112 s121 s122
 
-        val s111 = State("s111")
-        val s112 = State("s112")
-        val s11 = State("s11")
+        val s111 = State("s111", parentId = listOf("s11"))
+        val s112 = State("s112", parentId = listOf("s11"))
+        val s11 = State("s11", parentId = listOf("s1"))
 
-        val s121 = State("s121")
-        val s122 = State("s122")
-        val s12 = State("s12", initial = true)
+        val s121 = State("s121", parentId = listOf("s12"))
+        val s122 = State("s122", parentId = listOf("s12"))
+        val s12 = State("s12", parentId = listOf("s1"), initial = true)
 
         val s1 = State("s1")
 
-        val s21 = State("s21")
+        val s21 = State("s21", parentId = listOf("s2"))
         val s2 = State("s2", initial = true)
 
-        init {
-            s111.parent = s11
-            s112.parent = s11
+        val s3 = State("s3")
 
-            s121.parent = s12
-            s122.parent = s12
+        val s3_s2 = State("s2", parentId = listOf("s3"))
+        val s3_s12 = State("s12", parentId = listOf("s3"))
 
-            s11.parent = s1
-            s12.parent = s1
-
-            s21.parent = s2
-
-            s2.subStates = listOf(s21)
-            s1.subStates = listOf(s11, s12)
-
-            s11.subStates = listOf(s111, s112)
-            s12.subStates = listOf(s121, s122)
-        }
+        val machine = Machine("test", listOf(s1, s2, s11, s12, s21, s111, s112, s121, s122, s3, s3_s2, s3_s12))
     }
 }
