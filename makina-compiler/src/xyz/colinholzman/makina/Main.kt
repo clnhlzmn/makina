211f9c92c4b112f24495808d4e9b2a5caf1e03e3
Colin <colin.holzman@protonmail.com>
2021-03-20 17:14:07 -0400

    Catch exceptions in main and print message.

--- makina-compiler/src/xyz/colinholzman/makina/Main.kt
+++ makina-compiler/src/xyz/colinholzman/makina/Main.kt
@@ -5,17 +5,20 @@ import java.nio.file.Paths
 
 fun main(args: Array<String>) {
     for (arg in args) {
-        println(arg)
-        val machine = Parse.file(arg)
-        val directoryPath = Paths.get(arg).parent.toString()
-        val headerPath = Paths.get(directoryPath, "${machine.id}.h").toString()
-        val sourcePath = Paths.get(directoryPath, "${machine.id}.c").toString()
-        val generator = CodeGenerator(machine)
-        File(headerPath).printWriter().use {
-            generator.generateHeader(it)
-        }
-        File(sourcePath).printWriter().use {
-            generator.generateSource(it)
+        try {
+            val machine = Parse.file(arg)
+            val directoryPath = Paths.get(arg).parent.toString()
+            val headerPath = Paths.get(directoryPath, "${machine.id}.h").toString()
+            val sourcePath = Paths.get(directoryPath, "${machine.id}.c").toString()
+            val generator = CodeGenerator(machine)
+            File(headerPath).printWriter().use {
+                generator.generateHeader(it)
+            }
+            File(sourcePath).printWriter().use {
+                generator.generateSource(it)
+            }
+        } catch (e: RuntimeException) {
+            println(e.localizedMessage)
         }
     }
 }
