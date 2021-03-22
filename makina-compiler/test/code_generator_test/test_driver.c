3761beba28482f06bc90685118d4ffc7626481d8
Colin <colin.holzman@protonmail.com>
2021-03-22 12:06:16 -0400

    Add -Werror to code generator test.

--- makina-compiler/test/code_generator_test/test_driver.c
+++ makina-compiler/test/code_generator_test/test_driver.c
@@ -5,66 +5,79 @@
 FILE *output;
 
 int s1_s2_entry(struct test *self, struct test_event *event) {
+    (void)self; (void)event;
     fprintf(output, "s1_s2_entry\n");
     return 0;
 }
 
 int s1_s2_e1_guard(struct test *self, struct test_event *event) {
+    (void)self; (void)event;
     fprintf(output, "s1_s2_e1_guard\n");
     return event->ctx.guard;
 }
 
 int s1_s2_e1_action(struct test *self, struct test_event *event) {
+    (void)self; (void)event;
     fprintf(output, "s1_s2_e1_action\n");
     return 0;
 }
 
 int s1_s2_exit(struct test *self, struct test_event *event) {
+    (void)self; (void)event;
     fprintf(output, "s1_s2_exit\n");
     return 0;
 }
 
 int s1_entry(struct test *self, struct test_event *event) {
+    (void)self; (void)event;
     fprintf(output, "s1_entry\n");
     return 0;
 }
 
 int s1_exit(struct test *self, struct test_event *event) {
+    (void)self; (void)event;
     fprintf(output, "s1_exit\n");
     return 0;
 }
 
 int s2_s3_entry(struct test *self, struct test_event *event) {
+    (void)self; (void)event;
     fprintf(output, "s2_s3_entry\n");
     return 0;
 }
 
 int s2_s3_exit(struct test *self, struct test_event *event) {
+    (void)self; (void)event;
     fprintf(output, "s2_s3_exit\n");
     return 0;
 }
 
 int s2_s1_entry(struct test *self, struct test_event *event) {
+    (void)self; (void)event;
     fprintf(output, "s2_s1_entry\n");
     return 0;
 }
 
 int s2_s1_e1_action(struct test *self, struct test_event *event) {
+    (void)self; (void)event;
     fprintf(output, "s2_s1_e1_action\n");
     return 0;
 }
 
 int s2_s1_exit(struct test *self, struct test_event *event) {
+    (void)self; (void)event;
     fprintf(output, "s2_s1_exit\n");
     return 0;
 }
 
 int s2_entry(struct test *self, struct test_event *event) {
+    (void)self; (void)event;
     fprintf(output, "s2_entry\n");
     return 0;
 }
 
 int s2_exit(struct test *self, struct test_event *event) {
+    (void)self; (void)event;
     fprintf(output, "s2_exit\n");
     return 0;
 }
